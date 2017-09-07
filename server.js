39e249b230cd8c5141e1437cc181e6393f455f28
Hemcharan <maddenenihemcharan@gmail.com>
2017-09-07 20:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,9 +90,7 @@ app.get('/:articleName', function(req, res){
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
-app.get('/ui/style.css',function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+app.use(express.static('ui'));
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
