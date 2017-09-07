05dade1e042c0c33d70c8f68d1ee641030be5ca2
Hemcharan <maddenenihemcharan@gmail.com>
2017-09-07 20:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ function createTemplate(data){
     </html>`;
     return htmlTemplate;
 }
-app.get('/ui/index.htmll',function (req, res) {
+app.get('/ui/index.html',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/:articleName', function(req, res){
