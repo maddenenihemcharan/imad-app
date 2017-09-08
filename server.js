05b6cefa3d2b67c6716d0bdcdc3d093527dcd2a9
Hemcharan <maddenenihemcharan@gmail.com>
2017-09-08 16:43:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,8 +89,8 @@ app.get('/',function (req, res) {
 app.get('/ui/style.css',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/ui/imad.png',function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'imad.png'));
+app.get('/ui/madi.png',function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 app.get('/:articleName', function(req, res){
