b6822a18a9efa45a2618d53fbbb6ad3689fa85ff
Hemcharan <maddenenihemcharan@gmail.com>
2017-09-08 17:24:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML="NEW VALUE";
+
+var img=document.getElementById("madi.png");
+img.onclick=function(){
+    img.style.marginLeft="100px";
+    
+};
